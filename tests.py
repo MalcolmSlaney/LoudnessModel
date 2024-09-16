3ee119ecbf9959678f69344e7bbf5d0c5c8c7b35
Jeremy Hsiao <162384869+jerhsiao@users.noreply.github.com>
2024-09-16 16:23:08 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,6 +2,9 @@ from tvl2018 import *
 # to specify rate, add rate = your_rate_value in the parameters
 # to specify output_path, add output_path = your_output_path in the parameters
 
+# Add more tests
+
+# example usage 
 filename_or_sound = 'synthesize_1khz_100ms' # filename_or_sound must be a string, numpy array, or one of the provided synthesized sounds ('synthesize_{}khz_{}ms')
 db_max = 50
 filename_filter = 'transfer functions/ff_32000.mat'
