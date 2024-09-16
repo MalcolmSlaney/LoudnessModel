c0e815862caa2c39540a179d246e80338b6e81f3
Jeremy Hsiao <162384869+jerhsiao@users.noreply.github.com>
2024-09-16 12:51:41 -0700

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,9 @@
+from tvl2018 import *
+# to specify rate, add rate = your_rate_value in the parameters
+# to specify output_path, add output_path = your_output_path in the parameters
+
+filename_or_sound = 'synthesize_1khz_100ms' # filename_or_sound must be a string, numpy array, or one of the provided synthesized sounds ('synthesize_{}khz_{}ms')
+db_max = 50
+filename_filter = 'transfer functions/ff_32000.mat'
+loudness, short_term_loudness, long_term_loudness = main_tv2018(filename_or_sound, db_max, filename_filter)
+
