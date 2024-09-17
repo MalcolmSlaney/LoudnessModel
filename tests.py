30ed25d815b9157a066dfa482e0da72d7238e56e
Jeremy Hsiao <162384869+jerhsiao@users.noreply.github.com>
2024-09-17 15:14:04 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -4,9 +4,10 @@ from tvl2018 import *
 
 # Add more tests
 
-# example usage 
-filename_or_sound = 'synthesize_1khz_100ms' # filename_or_sound must be a string, numpy array, or one of the provided synthesized sounds ('synthesize_{}khz_{}ms')
-db_max = 50
-filename_filter = 'transfer functions/ff_32000.mat'
-loudness, short_term_loudness, long_term_loudness = main_tv2018(filename_or_sound, db_max, filename_filter)
-
+def test_loudness_model():
+    filename_or_sound = 'synthesize_1khz_100ms' # filename_or_sound must be a string, numpy array, or one of the provided synthesized sounds ('synthesize_{}khz_{}ms')
+    db_max = 50
+    filename_filter = 'transfer functions/ff_32000.mat'
+    loudness, short_term_loudness, long_term_loudness = main_tv2018(filename_or_sound, db_max, filename_filter)
+    print("Test completed successfully. Figure and results text file saved in results folder.")
+    
